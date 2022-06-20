42dc371f6fbcc00d3fa35b0445652fb6bafa0e78
Kjetil Haugland <haugkjet@gmail.com>
2022-06-20 10:00:09 +0200

    Testing some random number stuff in normal shader

--- src/client/shaders/normal/fragment.ts
+++ src/client/shaders/normal/fragment.ts
@@ -1,11 +1,20 @@
 const _FS = /*glsl*/ `
 
+vec3 hashOld33( vec3 p )
+{
+	p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
+			  dot(p,vec3(269.5,183.3,246.1)),
+			  dot(p,vec3(113.5,271.9,124.6)));
+
+	return fract(sin(p)*43758.5453123);
+}
+
 uniform vec3 sphereColor;
 varying vec3 v_Normal;
 uniform vec2 u_resolution;
 void main() { 
   //gl_FragColor = vec4(v_Normal,1.0);
-  gl_FragColor = vec4(sphereColor* v_Normal, 1.0);
+  gl_FragColor = vec4(hashOld33 (sphereColor* v_Normal), 1.0);
 
 }
 `;
--- src/client/shaders/normal/vertex.ts
+++ src/client/shaders/normal/vertex.ts
@@ -1,8 +1,18 @@
 const _VS = /* glsl */ `
 
+
+vec3 hashOld33( vec3 p )
+{
+	p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
+			  dot(p,vec3(269.5,183.3,246.1)),
+			  dot(p,vec3(113.5,271.9,124.6)));
+
+	return fract(sin(p)*43758.5453123);
+}
+
 varying vec3 v_Normal;
 void main() {
-  vec3 scale = vec3(0.5, 0.5, 0.5);
+  vec3 scale = hashOld33(position);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position * scale, 1.0);
   v_Normal = normal;
   
